3794812b2549ae9427e9750621fd57bd2cff1531
alwituke <alwituke@gmail.com>
2023-12-03 19:58:07 +0200

    responsive dizainas

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,25 @@
 
+
 new Splide( '.splide').mount();
 
 new Splide( '.splide', {
   type   : 'loop',
   perPage : 6,
+   breakpoints: {
+    1600:{perPage:4,},
+    1200:{perPage:3,},
+    800:{perPage:2,},
+    600:{perPage:1,},},
   drag   : 'free',
   focus  : 'center',
+  arrows: undefined,
   autoScroll: {
-         speed: 10,
+         speed: 8,
            },
 } ).mount( window.splide.Extensions );
+
+
+
+
+
+
